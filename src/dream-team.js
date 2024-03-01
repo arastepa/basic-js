5fcce8911938773e08ea40695131d8141d3eee57
arastepa <stepanyan2298@gmail.com>
2024-03-01 22:44:44 +0400

    init: add

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) { 
+  if (!Array.isArray(members)) {
+    return false;
+ }
+  const filteredNames = members
+  .filter(member => (typeof member === 'string'))
+  .map(name => name.trim().toUpperCase());
+
+  filteredNames.sort();
+  return filteredNames.map(el => el[0]).join('');
 }
 
 module.exports = {
--- src/what-season.js
+++ src/what-season.js
@@ -11,9 +11,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
+function getSeason(date) {
   throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+  // if (date === undefined)
+  //   return 'Unable to determine the time of year!';
+  // if (!(date instanceof Date))
+  //   throw new Error('Invalid date!');
+  // const res = date.getMonth();
+  // if (res < 2 || res === 11)
+  //   return 'winter';
+  // else if (res >=2 && res <= 4)
+  //   return 'spring';
+  // else if (res > 4 && res <= 7)
+  //   return 'summer';
+  // else if (res > 7 && res <= 10)
+  //   return 'fall';
 }
 
 module.exports = {
