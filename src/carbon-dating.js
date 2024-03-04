397d9c2065173f5083e9eecbb6505a24fa2eb4cf
arastepa <stepanyan2298@gmail.com>
2024-03-04 21:21:32 +0400

    feat: new

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,13 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  const parsed = Number.parseFloat(sampleActivity);
+  if (typeof sampleActivity !== 'string' || isNaN(parsed) || parsed <= 0 || parsed > MODERN_ACTIVITY) {
+    return false;
+  }
+  const age = Math.ceil(Math.log(MODERN_ACTIVITY / parsed) / (0.693 / HALF_LIFE_PERIOD));
+  return age;
 }
 
 module.exports = {
