2429318e81c569792c11ede23fa58cedd56a5246
arastepa <stepanyan2298@gmail.com>
2024-03-04 21:05:18 +0400

    feat: new

--- src/what-season.js
+++ src/what-season.js
@@ -12,20 +12,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 function getSeason(date) {
-  throw new NotImplementedError('Not implemented');
-  // if (date === undefined)
-  //   return 'Unable to determine the time of year!';
-  // if (!(date instanceof Date))
-  //   throw new Error('Invalid date!');
-  // const res = date.getMonth();
-  // if (res < 2 || res === 11)
-  //   return 'winter';
-  // else if (res >=2 && res <= 4)
-  //   return 'spring';
-  // else if (res > 4 && res <= 7)
-  //   return 'summer';
-  // else if (res > 7 && res <= 10)
-  //   return 'fall';
+  if (date === undefined)
+    return 'Unable to determine the time of year!';
+  if (!(date instanceof Date) || date[Symbol.toStringTag] === 'Date')
+    throw new Error('Invalid date!');
+  const res = date.getMonth();
+  if (res < 2 || res === 11)
+    return 'winter';
+  else if (res >=2 && res <= 4)
+    return 'spring';
+  else if (res > 4 && res <= 7)
+    return 'summer';
+  else if (res > 7 && res <= 10)
+    return 'fall';
 }
 
 module.exports = {
